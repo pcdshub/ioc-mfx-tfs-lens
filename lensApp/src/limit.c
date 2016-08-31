46ef066b1ad378e5765ff93b3b61b9ea7d564a37
Theodore Rendahl <trendahl@slac.stanford.edu>
2016-08-31 22:52:43 +0000

    Update screens and andded flexibel/fixed limit distinction
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/lens/current@26621 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ lensApp/src/limit.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+#include <dbEvent.h>
+#include <dbDefs.h>
+#include <dbCommon.h>
+#include <registryFunction.h>
+#include <epicsExport.h>
+#include <recSup.h>
+#include <genSubRecord.h>
+#include <lensLimit.h>
+/* 
+    Look up a value from a pre-calculated table the low and/or high 
+        INPUTS
+            E = User input fundamental energy (eV)
+            T = Type of limit evaluation, as defined in lensLimit.h
+
+        OUTPUTS
+            L = Lower limit of the acceptable operation range
+            H = Higher limit of the acceptable operation range
+*/
+double find_limits(int l,int u, double E_list[], double L_list[],  double E)
+{
+    int m;
+    double limit;
+    while(l < u) {
+        m = (l+u) /2;
+        if( E==E_list[l] ) {
+            u = l;
+        }
+        else if( E==E_list[m] ) {
+            u = m;
+            l = m;
+        }
+        else if( E == E_list[u] ){
+            l = u;
+        }
+        else if( E < E_list[m] ) {
+            if( u == m ) break;
+            u = m;
+        } else {
+           if ( l == m ) break;
+          l = m;
+        }
+    }
+    double slope = 0.;
+    if (u !=l) {
+        slope = (L_list[u]-L_list[l])/(E_list[u]-E_list[l]);
+    }
+    limit  = L_list[l] + slope*(E-E_list[l]);
+    return limit;
+
+}
+
+
+long limit_gensub_init(genSubRecord *pgsub)
+{
+    double E; /* Current beam energy*/
+    E = *(double *)pgsub->e;
+    return 0;
+}
+
+long limit_gensub_process(genSubRecord *pgsub)
+{
+    double E;   /*Current beam energy for first harmonic*/
+    double L;   /*Low range of acceptable limit*/
+    double H;   /*High range of acceptable limit*/ 
+    
+    
+    /*Check status of record*/
+    if( pgsub == NULL)
+        return -1;
+
+    /*Retrieve current beam energy (eV)*/
+    E = (*(double *)pgsub->e);
+    char* T = (char*) pgsub->t;
+
+    if(strcmp(T,MFX_ONLY)==0) {
+        L = -10000.;
+        H = find_limits(0,99,Etab,MFX_only,E);
+    }
+    else if(strcmp(T,XRT_ONLY)==0) {
+        L = find_limits(0,99,Etab,XRT_only,E);
+        H = 10000.;
+    }
+    else if(strcmp(T,PREFOCUS_MFX)==0) {
+        L = -10000.;
+        H = find_limits(0,99,Etab,Prefocus,E);
+    }
+    else {
+            L = 0.;
+            H = 0.;
+    }
+
+    *(double *)pgsub->vall = L;
+    *(double *)pgsub->valh = H;
+
+    pgsub->udf = FALSE;
+
+    return 1;
+}
+epicsRegisterFunction(limit_gensub_init);
+epicsRegisterFunction(limit_gensub_process);
