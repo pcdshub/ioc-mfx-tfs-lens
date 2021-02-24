b2288939f770a4b2dd691ceaebb227fbf5bd71ec
Ken Lauer <klauer@slac.stanford.edu>
2021-02-23 16:40:33 -0800

    MNT: add generated PLC code + Makefile rules

--- table_generator/dump_tables.c
+++ table_generator/dump_tables.c
@@ -11,30 +11,32 @@
 void dump_tables() {
     RangeRow value;
 
-	const RangeTable *tables[] = {
-		&TABLE_NO_LENS, &TABLE_LENS3_333, &TABLE_LENS2_428, &TABLE_LENS1_750
-	};
+    const RangeTable *tables[] = {
+        &TABLE_NO_LENS, &TABLE_LENS3_333, &TABLE_LENS2_428, &TABLE_LENS1_750
+    };
 
-	const char *table_name;
+    const char *table_name;
+    int table_idx;
+    double energy;
 
-	for (int table_idx=0; table_idx < 4; table_idx++) {
-		table_name = tables[table_idx]->table_name;
-		FILE *fp = fopen(table_name, "wt");
-		if (!fp) {
-			fprintf(stderr, "Failed to open %s\n", table_name);
-			continue;
-		}
-		fprintf(fp, "energy,low,high\n");
-		for (double energy=0.0; energy < 40000.0; energy += 1.0) {
-			if (find_limits(&value, tables[table_idx], energy)) {
-				print_row(fp, value, true);
-			}
-		}
-		fclose(fp);
-	}
+    for (table_idx=0; table_idx < 4; table_idx++) {
+        table_name = tables[table_idx]->table_name;
+        FILE *fp = fopen(table_name, "wt");
+        if (!fp) {
+            fprintf(stderr, "Failed to open %s\n", table_name);
+            continue;
+        }
+        fprintf(fp, "energy,low,high\n");
+        for (energy=0.0; energy < 40000.0; energy += 1.0) {
+            if (find_limits(&value, tables[table_idx], energy)) {
+                print_row(fp, value, true);
+            }
+        }
+        fclose(fp);
+    }
 }
 
 int main(void) {
-	dump_tables();
-	return 0;
+    dump_tables();
+    return 0;
 }
