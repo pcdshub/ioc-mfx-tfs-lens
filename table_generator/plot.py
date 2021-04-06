9619ddfa3eb5de77a9063e32871989d22a12ad94
Ken Lauer <klauer@slac.stanford.edu>
2021-04-06 14:46:51 -0700

    MNT: fix up makefile

---
+++ table_generator/plot.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+for fn in sys.argv[1:]:
+    if fn.endswith('.pdf'):
+        continue
+    df = pd.read_csv(fn)
+    df = df.set_index(df.energy)
+    plt.figure()
+    plt.plot(df.energy, df.low)
+    plt.plot(df.energy, df.high)
+    plt.yscale('log')
+    plt.title(fn)
+    plt.savefig(f'{fn}.pdf')
